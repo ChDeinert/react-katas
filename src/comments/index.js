6e49b5ff0188215718f61c7abbffab0aeb157c97
Christian Deinert <ChDeinert@gmail.com>
2017-05-28 14:56:06 +0200

    Add commentForm behaviour to comments

--- src/comments/index.js
+++ src/comments/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import shortid from 'shortid';
 import CommentList from './commentList';
 import Comment from './comment';
 import CommentForm from './commentForm';
@@ -7,32 +8,73 @@ import CommentForm from './commentForm';
 class Comments extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showForm: props.commentList.length > 0 ? false : true,
+      commentList: props.commentList,
+    };
+    this.submitForm = this.submitForm.bind(this);
+    this.cancelForm = this.cancelForm.bind(this);
   }
+
   render() {
+    const { showForm, commentList } = this.state;
+    const comments = commentList.map(currentComment => (
+      <Comment commentItem={currentComment} key={shortid.generate()} />
+    ));
+
     return (
       <section>
-        <CommentForm />
+        {showForm
+          ? <CommentForm submit={this.submitForm} cancel={this.cancelForm} />
+          : <div className="comments__form-wrapper">
+              <button
+                className="comments__form-button"
+                onClick={() => this.setState({ showForm: true })}
+              >
+                Add Comment
+              </button>
+            </div>}
         <CommentList>
-          <Comment
-            commentItem={{
-              name: 'Christian Deinert',
-              mail: 'ChDeinert@gmail.com',
-              content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            }}
-          />
-          <Comment
-            commentItem={{
-              name: 'Christian Deinert',
-              mail: 'ChDeinert@gmail.com',
-              content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            }}
-          />
+          {comments}
         </CommentList>
       </section>
     );
   }
+
+  submitForm(event) {
+    event.preventDefault();
+
+    const newComment = {
+      name: event.target.name.value,
+      mail: event.target.mail.value,
+      content: event.target.mail.value,
+    };
+    const newCommentList = this.state.commentList;
+    newCommentList.push(newComment);
+    this.setState({
+      commentList: newCommentList,
+      showForm: false,
+    });
+  }
+
+  cancelForm() {
+    this.setState({
+      showForm: false,
+    });
+  }
 }
 
-Comments.propTypes = {};
+Comments.propTypes = {
+  commentList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      mail: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ),
+};
+Comments.defaultProps = {
+  commentList: [],
+};
 
 export default Comments;
