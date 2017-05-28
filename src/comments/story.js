8acad6980c1b51a1664ac8e09b533660c52633b5
Christian Deinert <ChDeinert@gmail.com>
2017-05-28 14:54:03 +0200

    Add Comments-story cases

--- src/comments/story.js
+++ src/comments/story.js
@@ -3,4 +3,25 @@ import { storiesOf } from '@kadira/storybook';
 import Comments from './';
 import './style.sass';
 
-storiesOf('Comments', module).add('minimal', () => <Comments />);
+storiesOf('Comments', module)
+  .add('empty', () => <Comments />)
+  .add('with comments', () => {
+    const comments = [
+      {
+        name: 'Christian Deinert',
+        mail: 'ChDeinert@gmail.com',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+      {
+        name: 'Christian Deinert again',
+        mail: 'ChDeinert@gmail.com',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+      {
+        name: 'Christian Deinert and again',
+        mail: 'ChDeinert@gmail.com',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+    ];
+    return <Comments commentList={comments} />;
+  });
