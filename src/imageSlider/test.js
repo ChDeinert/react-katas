d75d11503bbc8f2a2538d5edcc98601e4f380102
Christian Deinert <ChDeinert@gmail.com>
2017-07-02 11:12:21 +0200

    Add description wrappers to tests

--- src/imageSlider/test.js
+++ src/imageSlider/test.js
@@ -3,53 +3,55 @@ import { shallow, mount } from 'enzyme';
 
 import ImageSlider from './';
 
-it('renders without images', () => {
-  const wrapper = shallow(<ImageSlider images={[]} />);
-  expect(wrapper.find('SliderControls').length).toBe(1);
-  expect(wrapper.find('SliderControls').props().previous).toBeDefined();
-  expect(wrapper.find('SliderControls').props().next).toBeDefined();
-  expect(wrapper.find('SliderItemList').length).toBe(1);
-  expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(0);
-  expect(wrapper.find('SliderIndicators').length).toBe(1);
-  expect(wrapper.find('SliderIndicators').props().itemcount).toBe(0);
-  expect(wrapper.find('SliderIndicators').props().click).toBeDefined();
-});
+describe('ImageSlider', () => {
+  it('renders without images', () => {
+    const wrapper = shallow(<ImageSlider images={[]} />);
+    expect(wrapper.find('SliderControls').length).toBe(1);
+    expect(wrapper.find('SliderControls').props().previous).toBeDefined();
+    expect(wrapper.find('SliderControls').props().next).toBeDefined();
+    expect(wrapper.find('SliderItemList').length).toBe(1);
+    expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(0);
+    expect(wrapper.find('SliderIndicators').length).toBe(1);
+    expect(wrapper.find('SliderIndicators').props().itemcount).toBe(0);
+    expect(wrapper.find('SliderIndicators').props().click).toBeDefined();
+  });
 
-it('renders with single image', () => {
-  const wrapper = shallow(
-    <ImageSlider images={[{ src: 'foo', alt: 'bar' }]} />
-  );
-  expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(1);
-  expect(wrapper.find('SliderIndicators').props().itemcount).toBe(1);
-});
+  it('renders with single image', () => {
+    const wrapper = shallow(
+      <ImageSlider images={[{ src: 'foo', alt: 'bar' }]} />
+    );
+    expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(1);
+    expect(wrapper.find('SliderIndicators').props().itemcount).toBe(1);
+  });
 
-it('renders with multiple images', () => {
-  const wrapper = shallow(
-    <ImageSlider
-      images={[
-        { src: 'foo', alt: 'bar' },
-        { src: 'foo2', alt: 'bar2' },
-        { src: 'foo3', alt: 'bar3' },
-      ]}
-    />
-  );
-  expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(3);
-  expect(wrapper.find('SliderIndicators').props().itemcount).toBe(3);
-});
+  it('renders with multiple images', () => {
+    const wrapper = shallow(
+      <ImageSlider
+        images={[
+          { src: 'foo', alt: 'bar' },
+          { src: 'foo2', alt: 'bar2' },
+          { src: 'foo3', alt: 'bar3' },
+        ]}
+      />
+    );
+    expect(wrapper.find('SliderItemList').props().itemlist.length).toBe(3);
+    expect(wrapper.find('SliderIndicators').props().itemcount).toBe(3);
+  });
 
-it('sets the correct state on changeCurrentImage', () => {
-  const wrapper = mount(
-    <ImageSlider
-      images={[
-        { src: 'foo', alt: 'bar' },
-        { src: 'foo2', alt: 'bar2' },
-        { src: 'foo3', alt: 'bar3' },
-      ]}
-    />
-  );
-  expect(wrapper.state().currentImage).toBe(0);
-  wrapper.find('SliderIndicators').childAt(1).simulate('click');
-  expect(wrapper.state().currentImage).toBe(1);
-  wrapper.find('SliderIndicators').childAt(2).simulate('click');
-  expect(wrapper.state().currentImage).toBe(2);
+  it('sets the correct state on changeCurrentImage', () => {
+    const wrapper = mount(
+      <ImageSlider
+        images={[
+          { src: 'foo', alt: 'bar' },
+          { src: 'foo2', alt: 'bar2' },
+          { src: 'foo3', alt: 'bar3' },
+        ]}
+      />
+    );
+    expect(wrapper.state().currentImage).toBe(0);
+    wrapper.find('SliderIndicators').childAt(1).simulate('click');
+    expect(wrapper.state().currentImage).toBe(1);
+    wrapper.find('SliderIndicators').childAt(2).simulate('click');
+    expect(wrapper.state().currentImage).toBe(2);
+  });
 });
--- src/pricing/pricingSection/test.js
+++ src/pricing/pricingSection/test.js
@@ -3,34 +3,36 @@ import { shallow } from 'enzyme';
 
 import PricingSection from './';
 
-it('Renders empty', () => {
-  const wrapper = shallow(<PricingSection />);
-  expect(wrapper.find('.pricing').length).toBe(1);
-  expect(wrapper.children().length).toBe(0);
-});
+describe('PricingSection', () => {
+  it('Renders empty', () => {
+    const wrapper = shallow(<PricingSection />);
+    expect(wrapper.find('.pricing').length).toBe(1);
+    expect(wrapper.children().length).toBe(0);
+  });
 
-it('Renders with one child', () => {
-  const wrapper = shallow(
-    <PricingSection><div className="one">foo</div></PricingSection>
-  );
-  expect(wrapper.children().length).toBe(1);
-  expect(wrapper.children().props().className).toBe('one');
-  expect(wrapper.children().children().text()).toBe('foo');
-});
+  it('Renders with one child', () => {
+    const wrapper = shallow(
+      <PricingSection><div className="one">foo</div></PricingSection>
+    );
+    expect(wrapper.children().length).toBe(1);
+    expect(wrapper.children().props().className).toBe('one');
+    expect(wrapper.children().children().text()).toBe('foo');
+  });
 
-it('Renders with multiple children', () => {
-  const wrapper = shallow(
-    <PricingSection>
-      <div className="one">foo</div>
-      <div className="two">bar</div>
-      <div className="three">foobar</div>
-    </PricingSection>
-  );
-  expect(wrapper.children().length).toBe(3);
-  expect(wrapper.children().at(0).props().className).toBe('one');
-  expect(wrapper.children().at(0).children().text()).toBe('foo');
-  expect(wrapper.children().at(1).props().className).toBe('two');
-  expect(wrapper.children().at(1).children().text()).toBe('bar');
-  expect(wrapper.children().at(2).props().className).toBe('three');
-  expect(wrapper.children().at(2).children().text()).toBe('foobar');
+  it('Renders with multiple children', () => {
+    const wrapper = shallow(
+      <PricingSection>
+        <div className="one">foo</div>
+        <div className="two">bar</div>
+        <div className="three">foobar</div>
+      </PricingSection>
+    );
+    expect(wrapper.children().length).toBe(3);
+    expect(wrapper.children().at(0).props().className).toBe('one');
+    expect(wrapper.children().at(0).children().text()).toBe('foo');
+    expect(wrapper.children().at(1).props().className).toBe('two');
+    expect(wrapper.children().at(1).children().text()).toBe('bar');
+    expect(wrapper.children().at(2).props().className).toBe('three');
+    expect(wrapper.children().at(2).children().text()).toBe('foobar');
+  });
 });
